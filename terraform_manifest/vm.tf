45cfaaee4e99ce66b9f31212fa6076dc06f94a2b
timepass <88819601+Aruhub@users.noreply.github.com>
2024-02-09 09:16:03 +0530

    Update vm.tf

--- terraform_manifest/vm.tf
+++ terraform_manifest/vm.tf
@@ -57,6 +57,17 @@ resource "azurerm_linux_virtual_machine" "mylinuxvm" {
         "sudo yum-config-manager --enable rhel-server-rhscl-7-rpms",
         "sudo yum install rh-php72",
         "scl enable rh-php72 bash",
+
+        "sudo apt-get update && sudo apt-get install docker.io -y",
+        "sudo mkdir /mnt/data && chmod 644 /mnt/data",
+        "sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && sudo chmod 755 ./kubectl && sudo mv ./kubectl /usr/local/bin/kubectl",
+        "curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/",
+        "sudo minikube start --vm-driver=none",
+        "sleep 30",
+        "sudo apt-get install git -y",
+        "sudo git clone https://github.com/Anurag-30/MediaWiki.git && cd MediaWiki && cd kubernetes && sudo kubectl create -f secrets.yaml -f persistent-volumes.yaml",
+        "sudo kubectl create  -f mariadb-deployment.yaml -f mariadb-svc.yaml",
+        "sudo kubectl create -f app-deployment.yaml -f web-service.yaml"
         ]
     connection {
       host     = data.azurerm_public_ip.pip.id
